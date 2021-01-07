882b6a1816d9047e2a0ac033b1b043862955e1fe
kamath7 <adithyakamath96@gmail.com>
2021-01-07 21:07:30 +0530

    Part1 - complete. Pending to add more bcrypt

--- securing-apis/index.js
+++ securing-apis/index.js
@@ -1,7 +1,27 @@
-const express = require('express');
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log(`Listening on ${PORT}`);
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology:true,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send(`Running lol`);
+});
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
---
+++ securing-apis/src/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+import bcrypt from "bcrypt";
+
+const Schema = mongoose.Schema;
+
+export const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  hashPassword: {
+    type: String,
+    required: true,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+UserSchema.methods.comparePassword = (password, hashPassword) => {
+  return bcrypt.compareSync(password, hashPassword); //comparing hashedpassword with db password
+};
