d3fb1bbf213015d1c0c15d8c76215e4003e2cf4e
kamath7 <adithyakamath96@gmail.com>
2021-01-11 01:26:21 +0530

    Stuck on some bug :(

--- templating-engines/index.js
+++ templating-engines/index.js
@@ -1,11 +1,37 @@
-const { getRates } = require("./services/getRates");
+const { getRates, getListOfCurrencies } = require("./services/getRates");
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const rate = await getRates("HKD", "INR");
-  await res.send({ rate });
+// app.get("/", async (req, res) => {
+//   const rate = await getRates("HKD", "INR");
+//   await res.send({ rate });
+// });
+app.use(bodyParser.json());
+app.set(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/currency", async (req, res) => {
+  const listofcurrencies = await getListOfCurrencies();
+  await res.render("home", {
+    name: "Kams!",
+    currencies: listofcurrencies,
+    amount: "",
+  });
+});
+
+app.post("/currency/getExchange", async (req, res) => {
+  console.log(req.body);
+  const listofcurrencies = await getListOfCurrencies();
+
+  res.render("home", {
+    name: "Kams!",
+    currencies: listofcurrencies,
+    amount: req.body.amount,
+  });
 });
 
 app.listen(5000, () => {
--- templating-engines/services/getRates.js
+++ templating-engines/services/getRates.js
@@ -5,8 +5,17 @@ const getRates = async (base, amt) => {
     `https://api.exchangeratesapi.io/latest?base=${base}`
   );
   const realRates = await rates.data.rates;
+
   const forexRate = await realRates[amt];
   return await forexRate;
 };
+const getListOfCurrencies = async () => {
+  const rates = await axios.get(
+    `https://api.exchangeratesapi.io/latest?base=INR`
+  );
+  const realRates = await rates.data.rates;
+  const listofcurrencies = await Object.keys(realRates);
+  return await listofcurrencies;
+};
 
-module.exports = { getRates };
+module.exports = { getRates, getListOfCurrencies };
